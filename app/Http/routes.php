c7f977636322b4e2569677ea1719fbb4f72eb690
joyful4yu <joyful4yu@gmail.com>
2015-06-15 17:11:45 +0800

    comit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,27 +25,47 @@ $router->post('send-sms-verification', function(Illuminate\Http\Request $request
 		return '{"status":"error","message":"手机号码格式不正确"}';
 	}
 	
-	// return '{"phone":"'. $mobile_number .'","checkCode":"123","status":"success"}';
+	return '{"object":{"phone":"'. $mobile_number .'","checkCode":"123"},"status":"success"}';
 
-	$url = 'http://www.zuulee.com/cgi-bin/user/sendVerifyCode.do';
-	$data = [
-		'phone' => $mobile_number,
-		'sendMode' => 2 ,
-		'codeType' => 1 ,
-		'location' => 10 
-	];
-	$postdata = http_build_query($data);
-	$opts = array('http' => [
-			'method'  => 'POST',
-			'header'  => 'Content-type: application/x-www-form-urlencoded',
-			'content' => $postdata
-		]
-	);
-	$context = stream_context_create($opts);
-	$result = file_get_contents($url, false, $context);
-	// $result = '{"v_code":"8447","errcode":0}';
-	return '{"phone":"'. $mobile_number .'","checkCode":"'. json_decode($result)->v_code .'","status":"success"}';
+	// $url = 'http://www.zuulee.com/cgi-bin/user/sendVerifyCode.do';
+	// $data = [
+	// 	'phone' => $mobile_number,
+	// 	'sendMode' => 2 ,
+	// 	'codeType' => 1 ,
+	// 	'location' => 10 
+	// ];
+	// $postdata = http_build_query($data);
+	// $opts = array('http' => [
+	// 		'method'  => 'POST',
+	// 		'header'  => 'Content-type: application/x-www-form-urlencoded',
+	// 		'content' => $postdata
+	// 	]
+	// );
+	// $context = stream_context_create($opts);
+	// $result = file_get_contents($url, false, $context);
+	// // $result = '{"v_code":"8447","errcode":0}';
+	// return '{"phone":"'. $mobile_number .'","checkCode":"'. json_decode($result)->v_code .'","status":"success"}';
 });
+
+// Login 
+
+$router->post('login', function(Illuminate\Http\Request $request)
+{
+	$username = $request->get('username');
+	$password = $request->get('password');
+	if(! preg_match('/^1[3458][0-9]{9}$/', $username)){
+		return '{"status":"error","message":"手机号码格式不正确"}';
+	}
+	$users = App\User::where('email', '=', $username)->get();
+	// dd($users[0]->password);
+	if(sizeof($users) > 0 && Hash::check($password,$users[0]->password)){
+		return '{"status":"success","message":"登陆成功"}';
+	}else{
+		return '{"status":"error","message":"用户名或密码错误"}';
+	}
+	
+});
+
 
 // Register User
 $router->post('register', function(Illuminate\Http\Request $request){
